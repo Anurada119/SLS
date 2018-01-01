c15297526fd1cc5d2a56ed882235ddbe9f4a2373
Anurada Gunarathna <anurada119@gmail.com>
2018-01-01 22:18:12 +0530

    done

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,58 @@
-console.log('wtf');
+$(document).ready(function(){
+    $('.js-slides').slick({
+        dots: true,
+        autoplay: true,
+        infinite: true,
+        arrows: false
+    });
+    $('.js-slides-2').slick({
+        dots: false,
+        autoplay: true,
+        infinite: true,
+        arrows: false
+    });
+    // $('.js-slides-3').slick({
+    //     dots: false,
+    //     autoplay: true,
+    //     infinite: true,
+    //     arrows: false,
+    //     slidesToShow: 3,
+    //     slidesToScroll: 1
+    // });
+    $('.js-slides-3').slick({
+        dots: false,
+        infinite: false,
+        speed: 300,
+        arrows: false,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 3,
+              infinite: true,
+              dots: false
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+          // You can unslick at a given breakpoint now by adding:
+          // settings: "unslick"
+          // instead of a settings object
+        ]
+      });
+  });
